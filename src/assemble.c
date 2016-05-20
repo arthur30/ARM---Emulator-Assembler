a6a3fda9adcb3237dafc88d62f0fa575e387ce0f
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-05-20 15:14:14 +0100

    IO File Handling
    Implemented methods to open file and catch all the errors.

--- src/assemble.c
+++ src/assemble.c
@@ -1,12 +1,60 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "instructions.h"
+
+struct symbol {
+	char *label;
+	uint16_t address;
+};
+
+int numLines(FILE *file)
+{
+	char ch;
+	int num_of_lines = 0;
+
+	do {
+		ch = fgetc(file);
+		if (ch == '\n')
+			num_of_lines++;
+	} while (ch != EOF);
+
+	if (ch != '\n' && num_of_lines != 0)
+		num_of_lines++;
+
+	return num_of_lines;
+}
 
 int main(int argc, char **argv)
 {
-	(void)argv;
+	FILE *input;
+	FILE *output;
+
+	/* Symbol Table ADT */
+	int symbol_table_size = 16;
+	struct symbol *symbol_table = malloc(symbol_table_size * sizeof(struct symbol));
 
 	/* Check if source and dest files are provided.	 */
 	if (argc < 3)
-		return 1;
+		return EXIT_FAILURE;
 
+	/* Open the input and output files */
+	input = fopen(argv[0], "r");
+	output = fopen(argv[1], "wb");
+
+	/* Handle errors if files don't exist */
+	if (input == NULL) {
+		perror("Error opening the input file");
+		return EXIT_FAILURE;
+	}
+
+	if (output == NULL) {
+		perror("Error opening the output file");
+		return EXIT_FAILURE;
+	}
+
+	
+
+	free(symbol_table);
 	return EXIT_SUCCESS;
 }
