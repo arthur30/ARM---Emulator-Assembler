832563742a99ed58faef52c20cc8fb29b6a254a3
Szilveszter Székely <szilveszter.szekely15@imperial.ac.uk>
2016-05-20 05:57:05 +0100

    Add working emulator

--- src/emulate.c
+++ src/emulate.c
@@ -1,99 +1,89 @@
+#include "emulate_pi_state.h"
+#include "emulate_fetch.h"
+#include "emulate_decode.h"
+#include "emulate_execute.h"
+
 #include <stdlib.h>
-#include <stdint.h>
+#include <stdio.h>
 #include <stdbool.h>
+#include <errno.h>
 
-#define R_PC 15
-
-struct instr_data_proc {
-	bool immediate;
-	uint8_t opcode;
-	uint8_t rn;
-	uint8_t rd;
-	uint16_t op2;
-};
-
-struct instr_mult {
-	bool accumulate;
-	bool setcond;
-	uint8_t rd;
-	uint8_t rn;
-	uint8_t rs;
-	uint8_t rm;
-};
-
-struct instr_transfer {
-	bool immediate;
-	bool preindexing;
-	bool up;
-	bool load;
-	uint8_t rn;
-	uint8_t rd;
-	uint16_t offset;
-};
-
-struct instr_branch {
-	uint32_t offset;
-};
-
-struct instr {
-	uint8_t cond;
-	uint8_t type;
-	union instr_bits_t {
-		struct instr_data_proc data_proc;
-		struct instr_mult mult;
-		struct instr_transfer transfer;
-		struct instr_branch branch;
-	} instr_bits;
-};
-
-struct pi_state {
-	uint8_t memory[1<<16];
-	uint32_t registers[16];
-	struct cpsr {
-		bool n;
-		bool z;
-		bool c;
-		bool v;
-	} cpsr;
-	struct pipeline {
-		bool fetched;
-		uint32_t instr_code;
-		struct instr instruction;
-	} pipeline;
-};
-
-static int fetch(struct pi_state *pstate)
+static void print_state(struct pi_state *pstate)
 {
-	(void)pstate;
-	return 0;
-}
+	fprintf(stdout, "Registers:\n");
+	for (int i = 0; i < 13; i++) {
+		uint32_t val = pstate->registers[i];
 
-static int decode(struct pi_state *pstate)
-{
-	(void)pstate;
-	return 0;
-}
+		fprintf(stdout, "$%-2d : %10d (0x%08x)\n", i, val, val);
+	}
 
-static int execute(struct pi_state *pstate)
-{
-	(void)pstate;
-	return 0;
+	uint32_t pc = pstate->registers[R_PC];
+
+	fprintf(stdout, "PC  : %10d (0x%08x)\n", pc, pc);
+
+	uint32_t cpsr = (pstate->cpsr.n << 31) |
+			(pstate->cpsr.z << 30) |
+			(pstate->cpsr.c << 29) |
+			(pstate->cpsr.v << 28);
+
+	fprintf(stdout, "CPSR: %10d (0x%08x)\n", cpsr, cpsr);
+
+	fprintf(stdout, "Non-zero memory:\n");
+
+	uint8_t *mem = pstate->memory;
+	size_t address = 0;
+
+	for (address = 0; address < PI_MEMORY_SIZE; address += 4) {
+		if (mem[address] |
+		    mem[address + 1] |
+		    mem[address + 2] |
+		    mem[address + 3])
+			fprintf(stdout, "0x%08zx: 0x%02x%02x%02x%02x\n",
+				address,
+				pstate->memory[address],
+				pstate->memory[address + 1],
+				pstate->memory[address + 2],
+				pstate->memory[address + 3]);
+	}
+
 }
 
 int main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	FILE *input;
 
 	struct pi_state *pstate = calloc(1, sizeof(struct pi_state));
 
-	while (true) {
-		if (pstate->pipeline.instruction.type)
-			execute(pstate);
-		if (pstate->pipeline.fetched)
-			decode(pstate);
-		fetch(pstate);
+	if (!pstate) {
+		fprintf(stderr, "Not enough memory for Pi state\n");
+		exit(1);
 	}
+
+	if (argc != 2) {
+		fprintf(stderr, "Provide an input binary as an argument\n");
+		exit(1);
+	}
+
+	input = fopen(argv[1], "rb");
+
+	if (!input)
+		fprintf(stderr, "Could not open input file\n");
+
+	fread(&pstate->memory, PI_MEMORY_SIZE, 1, input);
+
+	while (true) {
+		if (pstate->pipeline.decoded)
+			if (execute(pstate) && !errno)
+				break;
+		if (pstate->pipeline.fetched)
+			if (decode(pstate))
+				break;
+		if (fetch(pstate))
+			break;
+		pstate->registers[R_PC] += 4;
+	}
+
+	print_state(pstate);
 
 	return EXIT_SUCCESS;
 }
