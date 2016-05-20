dd0562736acd5c2ede484fc7a7806cb57993e81c
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-05-20 15:15:07 +0100

    Start Instructions Library
    A library to help parse the instructions from string to binary.

---
+++ src/instructions.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include "instructions.h"
+
+int instr_data_processing()
+{
+	return 0;
+}
+
+int instr_multiply()
+{
+	return 0;
+}
+
+int instr_single_data_transfer()
+{
+	return 0;
+}
+
+int instr_branch()
+{
+	return 0;
+}
---
+++ src/instructions.h
@@ -0,0 +1,12 @@
+#ifndef INSTRUCTIONS_H
+#define INSTRUCTIONS_H
+
+int instr_data_processing();
+
+int instr_multiply();
+
+int instr_single_data_transfer();
+
+int instr_branch();
+
+#endif
