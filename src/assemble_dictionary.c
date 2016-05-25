2b635ce472313eadf2399c1b05913006192ba1b9
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-05-25 14:27:18 +0100

    Move MAPS to correct file.
    Should build now.

--- src/assemble_dictionary.c
+++ src/assemble_dictionary.c
@@ -3,6 +3,39 @@
 #include <string.h>
 #include "assemble_dictionary.h"
 
+struct map {
+	const char *str;
+	int n;
+};
+
+struct map dict_dpi[] = {
+	{"and", 0},
+	{"eor", 1},
+	{"sub", 2},
+	{"rsb", 3},
+	{"add", 4},
+	{"orr", 12},
+	{"mov", 13},
+	{"tst", 8},
+	{"teq", 9},
+	{"cmp", 10},
+	{0, 0}
+};
+
+struct map dict_branch[] = {
+	{"beq", 0},
+	{"bne", 1},
+	{"bge", 10},
+	{"blt", 11},
+	{"bgt", 12},
+	{"ble", 13},
+	{"b", 14},
+	{"bal", 14},
+	{0, 0}
+};
+
+
+
 uint32_t dpi_to_opcode(char *key)
 {
 	int i = 0;
--- src/assemble_dictionary.h
+++ src/assemble_dictionary.h
@@ -1,37 +1,6 @@
 #ifndef ASSEMBLE_DICTIONARY_H
 #define ASSEMBLE_DICTIONARY_H
 
-struct map {
-	const char *str;
-	int n;
-};
-
-struct map dict_dpi[] = {
-	{"and", 0},
-	{"eor", 1},
-	{"sub", 2},
-	{"rsb", 3},
-	{"add", 4},
-	{"orr", 12},
-	{"mov", 13},
-	{"tst", 8},
-	{"teq", 9},
-	{"cmp", 10},
-	{0, 0}
-};
-
-struct map dict_branch[] = {
-	{"beq", 0},
-	{"bne", 1},
-	{"bge", 10},
-	{"blt", 11},
-	{"bgt", 12},
-	{"ble", 13},
-	{"b", 14},
-	{"bal", 14},
-	{0, 0}
-};
-
 uint32_t dpi_to_opcode(char *key);
 
 uint32_t branch_to_cond(char *key);
