eeb002f7f9afd16e453f6d9bc7db93a1dc76b10a
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-06-01 00:36:42 +0100

    GPIO: Edit comments. Use tabs and more comments.

--- programs/gpio.s
+++ programs/gpio.s
@@ -1,33 +1,41 @@
-ldr r0, =0x20200004             ; Memory address of GPIO pins 10-19
-ldr r1, =0x20200028             ; Memory address for clearing the pins
-ldr r2, =0x2020001C             ; Memory address for turning on the pins
+; ARM assembly program to set and clear GPIO port 16 at regular time intervals.
+;
+; r0 => Memory address for accesing GPIO pins 10-19.
+; r1 => Memory address to clear the pins.
+; r2 => Memory address to set the pins.
+; r3 => To set and clear bit 16 for pin 16.
+; r4 => Loop variable.
 
-mov r3, #1                      ; r3 = 1
-lsl r3, #18                     ; Logical shift left by 18(last bit of pin 16 at memory address from r0) bits on r3
-str r3, [r0]                    ; Pin 16 set to output pin
+ldr r0, =0x20200004		; Memory address of GPIO pins 10-19
+ldr r1, =0x20200028		; Memory address to clear the pins
+ldr r2, =0x2020001C		; Memory address to set the pins
+
+mov r3, #1			; r3 = 1
+lsl r3, #18 			; lsl by 18(sets bit for pin 16)
+str r3, [r0]			; Accessed Pin 16
 
 turn_on:
-        mov r3, #1              ; r3 = 1
-        lsl r3, #16             ; Logical shift left by 16(sets the bit for pin 16 in the "turning on" memory address-r2)
-        str r3, [r2]            ; Pin 16 is turned on
+	mov r3, #1		; r3 = 1
+	lsl r3, #16		; lsl by 16(sets pin 16 at memory address-r2)
+	str r3, [r2]		; Pin 16 is set
 
 delay1:
-        mov r4, #0              ; r4 = 0
+	mov r4, #0		; r4 = 0
 next1:
-        cmp r4, #0x1000000      ; Compares r4 with a really big value in order to delay the pin and sets the flags
-        bge turn_off            ; If r4 > 0x1000000, jump to turning the led off
-        add r4, r4, #1          ; Increment the value held by r4
-        bal next1               ; Jump to next iteration of delay1
+	cmp r4, #0x1000000	; Compare r4 and 0x1000000 for the delay.
+	bge turn_off		; If greater or equal, jump to turn_off.
+	add r4, r4, #1		; r4++
+	bal next1		; Next iteration of delay1.
 
 turn_off:
-        mov r3, #1              ; r3 = 1
-        lsl r3, #16             ; Logical shift left by 16(sets the bit for pin 16 in the clearing memory address-r1)
-        str r3, [r1]            ; Pin 16 is turned off
+	mov r3, #1		; r3 = 1
+	lsl r3, #16		; lsl by 16(clears the bit for pin 16)
+	str r3, [r1]		; Cleared pin 16.
 
 delay2:
-        mov r4, #0              ; r4 = 0
+	mov r4, #0		; r4 = 0
 next2:
-        cmp r4, #0x1000000      ; Compares r4 with a really big value in order to delay the pin and sets the flags
-        bge turn_on             ; If r4 > 0x1000000, jump to turning the led on
-        add r4, r4, #1          ; Increment the value held by r4
-        bal next2               ; Jump to next iteration of delay2
+	cmp r4, #0x1000000	; Compare r4 and 0x1000000 for the delay.
+	bge turn_on		; If greater or equal, jump to turn_on.
+	add r4, r4, #1		; r4++
+	bal next2		; Next iteration of delay2.
